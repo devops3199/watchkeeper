033a913de509d1f38e6c732c4770d53036723419
Ray Jeong <devops3199@gmail.com>
2023-02-07 23:00:42 +0900

    [fix] client public

--- plugins/firebase.client.ts
+++ plugins/firebase.client.ts
@@ -7,13 +7,13 @@ export default defineNuxtPlugin((nuxtApp) => {
   const config = useRuntimeConfig();
 
   const firebaseConfig = {
-    apiKey: config.fbApiKey,
-    authDomain: config.fbAuthDomain,
-    projectId: config.fbProjectId,
-    storageBucket: config.fbStorageBucket,
-    messagingSenderId: config.fbMessagingSenderId,
-    appId: config.fbAppId,
-    measurementId: config.fbMeasurementId,
+    apiKey: config.public.fbApiKey,
+    authDomain: config.public.fbAuthDomain,
+    projectId: config.public.fbProjectId,
+    storageBucket: config.public.fbStorageBucket,
+    messagingSenderId: config.public.fbMessagingSenderId,
+    appId: config.public.fbAppId,
+    measurementId: config.public.fbMeasurementId,
   };
 
   const app = initializeApp(firebaseConfig);
