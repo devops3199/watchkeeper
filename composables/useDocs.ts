4f2b85e5be3e32dfce169b4f8b53697127c4ddf8
Ray Jeong <devops3199@gmail.com>
2023-03-05 22:03:53 +0900

    [feat] docs <isFetching>

--- composables/useDocs.ts
+++ composables/useDocs.ts
@@ -3,19 +3,28 @@ import {Brand} from '@/model/brand';
 
 export default function () {
   const {$firestore} = useNuxtApp();
+  const isFetching = ref(false);
 
   const brandsRef = collection($firestore, 'brands');
 
   const getBrands = async () => {
     const result: Array<Brand> = [];
-    const docs = await getDocs(brandsRef);
 
-    docs.forEach((doc) => {
-      result.push(doc.data() as Brand);
-    });
+    try {
+      isFetching.value = true;
+      const docs = await getDocs(brandsRef);
+
+      docs.forEach((doc) => {
+        result.push(doc.data() as Brand);
+      });
+    } catch (e) {
+      console.error({catch: e});
+    } finally {
+      isFetching.value = false;
+    }
 
     return result;
   };
 
-  return {getBrands};
+  return {getBrands, isFetching};
 }
